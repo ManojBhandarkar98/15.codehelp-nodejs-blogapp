7e7ba1ed7d10249e5e139a533198dc299cae53d3
ManojBhandarkar98 <manojbhandarkar98@gmail.com>
2025-01-25 13:12:15 +0530

    Episode - 4 -Backend - BlogApp - LikePost create likeposr and updated

--- controller/likeController.js
+++ controller/likeController.js
@@ -1,3 +1,31 @@
+const Post = require("../model/view/postModel")
+const Comment = require("../model/view/commentModel");
+
+//define route Handler
+exports.likePost = async (req, res) => {
+    try {
+        //fetch data from req body
+        const { post, user } = req.body;
+        //new comment object
+        const like = new Like({
+            post, user
+        });
+        //save new comment into db
+        const savedLike = await like.save();
+        //find the post by id, add new comment to its comments array
+        const updatedPost = await Post.findByIdAndUpdate(post, { $push: { likes: savedLike._id } }, { new: true })
+        .populate("likes").exec();
+
+        res.json({
+            post: updatedPost
+        })
+    }
+    catch (error) {
+        return res.status(500).json({
+            error: "Error while creating comment"
+        })
+    }
+}
 
 //define route Handler
 exports.dummyLink = (req, res) => {
--- routes/blog.js
+++ routes/blog.js
@@ -3,14 +3,14 @@ const router = express.Router();
 
 //import controller
 const { createPost,getAllPosts } = require("../controllers/postController");
-
-const { dummyLink } = require("../controller/LikeController");
+const { dummyLink,likePost } = require("../controller/LikeController");
 const { createCommentController } = require("../controller/CommentController");
 
 router.post("/posts/create", createPost);
 router.get("/dummyroute", dummyLink);
 router.post("/comments/create",createCommentController);
 router.get("/posts",getAllPosts);
+router.post("/likes/like", likePost);
 
 
 
