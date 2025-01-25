280ea67effc7aec0698d8cc7d71920dede775ba4
ManojBhandarkar98 <manojbhandarkar98@gmail.com>
2025-01-25 13:29:52 +0530

    Episode - 4 -Backend - BlogApp - unlikePost create delete and updated likes procedure

--- controller/likeController.js
+++ controller/likeController.js
@@ -14,19 +14,43 @@ exports.likePost = async (req, res) => {
         const savedLike = await like.save();
         //find the post by id, add new comment to its comments array
         const updatedPost = await Post.findByIdAndUpdate(post, { $push: { likes: savedLike._id } }, { new: true })
-        .populate("likes").exec();
+            .populate("likes").exec();
 
         res.json({
             post: updatedPost
         })
     }
     catch (error) {
-        return res.status(500).json({
-            error: "Error while creating comment"
+        return res.status(400).json({
+            error: "Error while like"
         })
     }
 }
 
+
+exports.unlikePost = async (req, res) => {
+    try {
+        //fetch data from req body
+        const { post, like } = req.body;
+
+        const deletedLike = await Like.findOneAndDelete({ post: post, _id: like });
+
+
+        const updatedPost = await Post.findByIdAndUpdate(post,
+            { $pull: { likes: deletedLike._id } },
+            { new: true });
+
+
+        res.json({
+            post: updatedPost
+        })
+    }
+    catch (error) {
+        return res.status(400).json({
+            error: "Error while unlike"
+        })
+    }
+}
 //define route Handler
 exports.dummyLink = (req, res) => {
     res.send("This is your dummy page");
